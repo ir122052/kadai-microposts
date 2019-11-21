ebeaeb79299d08ef4021f3bfdcc3b4c544696511
ir.122052 <longhuiyantian@gmail.com>
2019-11-21 16:32:38 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,9 +8,9 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc)
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
-      render 'toppage/index'
+      render 'toppages/index'
     end
   end
 
